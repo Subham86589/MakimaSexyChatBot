347f7bf80031c626018178a2b042fe460317225d
Subbu <167736439+Subham86589@users.noreply.github.com>
2025-01-27 17:46:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,28 +5,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-API_ID = int(getenv("API_ID", "6435225"))
+API_ID = int(getenv("API_ID", "27469215"))
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+API_HASH = getenv("API_HASH", "544b135cbf33923fed1a7c461cc95f18")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-OWNER_ID = int(getenv("OWNER_ID", "6922271843"))
+OWNER_ID = int(getenv("OWNER_ID", "7218905176"))
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-SUPPORT_GRP = getenv("SUPPORT_GRP", "THE_FRIENDZ")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Sukunaxupdatess")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-UPDATE_CHNL = getenv("UPDATE_CHNL", "ROY_EDITX")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "sukunaxsupport")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
-OWNER_USERNAME = getenv("OWNER_USERNAME", "HLO_PAPA")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "TheBabyPerson")
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
 BOT_USERNAME = getenv("BOT_USERNAME", None)
@@ -35,41 +35,41 @@ BOT_USERNAME = getenv("BOT_USERNAME", None)
 #### ❖ ──────⊱◈◈◈⊰────── ❖
 
 STICKER = [
-"CAACAgUAAx0Cd9xEawACEQlmLuvFijxhTZXjFbLPOsZFBoZzYQAC2AUAAkVZsVftrDRpvXZTAAE0BA",
-"CAACAgUAAx0Cd9xEawACEQhmLuvFeujHQdaxeDtr3MZThRXa1QACkgcAArZSuVfbJAABQq5pIc80BA",
-"CAACAgUAAx0Cd9xEawACEQdmLuvFQdwX-ySKIrmq-JPWItfhhgACwQUAAi9GuVfYV7lLP7xl4zQE",
-"CAACAgUAAx0Cd9xEawACEQVmLuuiUNrymw5wWSie-agvZ-_MdgACNAQAAi9GsFf3M2dSfxH-YDQE",
-"CAACAgUAAx0Cd9xEawACEQNmLuuKwCEUmunIPFoxUL1Kr2Dp1AAChQgAApAXsFeIwfQvrfbmjjQE",
-"CAACAgUAAx0Cd9xEawACEQJmLut22O_5LobAKvCBNlOHbCnQcQAC8gQAAmRQsVdeP26A2AJofzQE",
-"CAACAgUAAx0Cd9xEawACEQABZi7rYhnPjPsm_g37JvqoH7qB10gAAsgEAAJWgShXcBbC69nedAY0BA",
-"CAACAgUAAx0Cd9xEawACEP9mLutgBdWYCVPqQ_kvUGgYoNVIVwACrAYAAof0IFc6sUwgfJZw6zQE",
-"CAACAgEAAx0Cd9xEawACEPtmLusPo3kBvdEigRxbcqGOMSF9cgAC8wMAAqpT6UU55jSF8wAByTc0BA",
-"CAACAgEAAx0Cd9xEawACEPpmLusJTIEch-TXN5KsPkvdfnypNgACbwIAAkoY6UUP_O3RGOXeSTQE",
-"CAACAgEAAx0Cd9xEawACEPlmLusBSvWNswwz99iOXBMIos0s_QACGAMAAtfI6EX4deIoUongJDQE",
-"CAACAgEAAx0Cd9xEawACEPdmLuropCmTrN0Xv4_C7plvS45D3gACrwIAAqyx6EVOdFVb4d8VsDQE",
-"CAACAgUAAx0Cd9xEawACEOhmLurMc76ZYy9ZWB0dcuWfNJVSzQACLwUAAk-LuVelZAHYP-pxnTQE",
-"CAACAgUAAx0Cd9xEawACEOZmLuq8MMZnoz-txKJ9QEow9qDKxQACKwQAAvbXuVf7GDiuoypXFzQE",
-"CAACAgUAAx0Cd9xEawACEORmLuq3Mm3dzamR5W8JZhZHgbPWKwACJwcAAvQcsFefMIzhat8ZtDQE",
-"CAACAgUAAx0Cd9xEawACEONmLuqxMsLOLjCsMIf86_QuZH0AAaAAAusMAAIRzNhVUrENdULkjis0BA",
-"CAACAgUAAx0Cd9xEawACEOFmLuqryqMN4_7KPq_LLZNIq0OPEgACJAwAAm5mwVXkZ2Ycjy1rRjQE",
-"CAACAgUAAx0Cd9xEawACEN9mLuqlG8RAw-L8e1Pv3909WrYMhgACwBUAAh-sOVQ3vSSCUJbSYzQE",
+"CAACAgUAAyEFAASFQ-2LAAEMGL9nl3VMNDDDkOGqPH5GOaS4iANgsQACgw8AAukZ0FT1SMcdEtTZmzYE",
+"CAACAgUAAyEFAASFQ-2LAAEL20hnlh8r2PzF6tp_6-k6Ibm_n-r7XAACohEAAnJyyVRmsZ-vz6WNXTYE",
+"CAACAgUAAyEFAASFQ-2LAAEL1g1nlgtR9OI7TCGUpAVRL5AN_QtVUwACeRIAAvTYOFT04MTtbO-FBjYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGPFnl3Yrqa5P56vZZ108IYr1miusqwACHBEAAjAwyVRezogRf5ejDTYE",
+"CAACAgUAAyEFAASFQ-2LAAEMFiJnl2rrz4k8sI3tz9RTE-J87mqboQACtBIAArKeyFSo8dj8EMI2FzYE",
+"CAACAgUAAyEFAASFQ-2LAAEMFiNnl2rswRiy5cxhpf2aMtu3WXbOHgACZRMAAkwn0FQECQFg42315DYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGPJnl3Yspi72qzHF1IY8gkiXta0tUgACphEAAs570VR9WMXVeaxIWTYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGNpnl3XE-9RoyqqjKynSdrK552sa8wACnxQAAiPx0VRpRSQIgVRVMjYE",
+"CAACAgUAAyEFAASFQ-2LAAEL5nlnlmf5VuajHpEXLN0a4kfwPuZTCAACuBAAAreS0VQByNe30ccy7zYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGO9nl3Yp_CYQTCwcO8Xa3Zz9Ujij6QAC6w8AAqqx0VTsU2z02LxUMDYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGPBnl3YqK6UmiIRPaJbM1Hjc3r2xbgAC0xIAAgkb0VSJk2NSH_Ot5zYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGPNnl3YtdldSeRLuFDpvXyKUPCR4ZgAC6hIAAq0i0VRAEwmGTDtffDYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGPZnl3YuVG_cqu7y53wdw8RuUMi8VQAC-xIAAldJyVSqN3fOuJgSczYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGPRnl3YtAVwknCDvlIYnAdUHUa2zfwACDRIAAnFp0FQPaOTdd_kl8zYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGSVnl3bjta9luvbD7vyBSmEkB4dqAwACKhMAAt83yVRjXa_RQPs2qjYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGShnl3blMfHo1t-VgeFzyq5aVEvBywAC7BIAAo_S0FRSyW0O6G6FqzYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGSZnl3bk_5tAWrV3f-ww7STGbd4KpgACzw8AArAc0VSi6XmPG6HiSzYE",
+"CAACAgUAAyEFAASFQ-2LAAEMGSpnl3bmUPyVFcLNVpLdi6e0dOZIsAACSBEAAmwB0FR8vRaxeqT48DYE",
 ]
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
 
 IMG = [
-"https://graph.org/file/eaa3a2602e43844a488a5.jpg",
-"https://graph.org/file/b129e98b6e5c4db81c15f.jpg",
-"https://graph.org/file/3ccb86d7d62e8ee0a2e8b.jpg",
-"https://graph.org/file/df11d8257613418142063.jpg",
-"https://graph.org/file/9e23720fedc47259b6195.jpg",
-"https://graph.org/file/826485f2d7db6f09db8ed.jpg",
-"https://graph.org/file/ff3ad786da825b5205691.jpg",
-"https://graph.org/file/52713c9fe9253ae668f13.jpg",
-"https://graph.org/file/8f8516c86677a8c91bfb1.jpg",
-"https://graph.org/file/6603c3740378d3f7187da.jpg",
-"https://graph.org/file/66cb6ec40eea5c4670118.jpg",
-"https://graph.org/file/2e3cf4327b169b981055e.jpg",
+"https://files.catbox.moe/wzic2t.jpg",
+"https://files.catbox.moe/l8f2ub.jpg",
+"https://files.catbox.moe/xs4kch.jpg",
+"https://files.catbox.moe/vmrilv.jpg",
+"https://files.catbox.moe/1be9fq.jpg",
+"https://files.catbox.moe/67c7nd.jpg",
+"https://files.catbox.moe/xs4kch.jpg",
+"https://files.catbox.moe/67c7nd.jpg",
+"https://files.catbox.moe/34nehn.jpg",
+"https://files.catbox.moe/afj35l.jpg",
+"https://files.catbox.moe/e8z4pb.jpg",
+"https://files.catbox.moe/lvkq31.jpg",
 ]
 
 #### ❖ ──────⊱◈◈◈⊰────── ❖
